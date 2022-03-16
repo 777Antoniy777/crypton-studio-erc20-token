57d3c3a2b78075ba3393244f8459f7e87f09fe92
Anton Kuzmitsky <tat7106@gmail.com>
2022-03-16 13:14:36 +0600

    Обрабатывает prettier

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is Ownable {
-    string private constant name = 'TonyToken';
-    string private constant symbol = 'TT';
-    uint8 private constant decimals = 18;
+    string private constant NAME = "TonyToken";
+    string private constant SYMBOL = "TT";
+    uint8 private constant DECIMALS = 18;
     uint256 private totalCount;
 
     mapping(address => uint256) private balances;
     mapping(address => mapping(address => uint256)) private allowed;
-	
+
     constructor(uint256 tokenInitialCount) {
         totalCount = tokenInitialCount;
         balances[msg.sender] = tokenInitialCount;
@@ -30,7 +30,7 @@ contract Token is Ownable {
     }
 
     function transfer(address receiverAddress, uint256 value) public returns (bool success) {
-        require(value <= balances[msg.sender], 'Your balance is less than needle');
+        require(value <= balances[msg.sender], "Your balance is less than needle");
 
         balances[msg.sender] = balances[msg.sender] - value;
         balances[receiverAddress] = balances[receiverAddress] + value;
@@ -40,45 +40,52 @@ contract Token is Ownable {
         return true;
     }
 
-    function transferFrom(address senderAddress, address receiverAddress, uint256 value) public returns (bool success) {
-		require(value <= balances[senderAddress], 'Your balance is less than needle');
-		require(value <= allowed[senderAddress][msg.sender], 'You cant transfer because you have not enough permissions');
+    function transferFrom(
+        address senderAddress,
+        address receiverAddress,
+        uint256 value
+    ) public returns (bool success) {
+        require(value <= balances[senderAddress], "Your balance is less than needle");
+        require(
+            value <= allowed[senderAddress][msg.sender],
+            "You cant transfer because you have not enough permissions"
+        );
 
-		balances[senderAddress] = balances[senderAddress] - value;
-		balances[receiverAddress] = balances[receiverAddress] + value;
-		allowed[senderAddress][msg.sender] = allowed[senderAddress][msg.sender] - value;
+        balances[senderAddress] = balances[senderAddress] - value;
+        balances[receiverAddress] = balances[receiverAddress] + value;
+        allowed[senderAddress][msg.sender] = allowed[senderAddress][msg.sender] - value;
 
-		emit Transfer(senderAddress, receiverAddress, value);
-		
+        emit Transfer(senderAddress, receiverAddress, value);
+
         return true;
     }
 
     function approve(address spenderAddress, uint256 value) public returns (bool success) {
-		allowed[msg.sender][spenderAddress] = value;
-		
-		emit Approval(msg.sender, spenderAddress, value);
-		
+        allowed[msg.sender][spenderAddress] = value;
+
+        emit Approval(msg.sender, spenderAddress, value);
+
         return true;
     }
 
     function allowance(address allowerAddress, address spenderAddress) public view returns (uint256 remaining) {
         return allowed[allowerAddress][spenderAddress];
     }
-	
-	// additional
-	function mint(address ownerAddress, uint256 amount) public onlyOwner {
-		totalCount = totalCount + amount;
-		balances[ownerAddress] = balances[ownerAddress] + amount;
 
-		emit Transfer(address(0), ownerAddress, amount);
-	}
+    // additional
+    function mint(address ownerAddress, uint256 amount) internal onlyOwner {
+        totalCount = totalCount + amount;
+        balances[ownerAddress] = balances[ownerAddress] + amount;
 
-	function burn(address ownerAddress, uint256 amount) public onlyOwner {
-		require(amount <= balances[ownerAddress], 'Tokens balance is less than burning tokens value');
-		
-		totalCount = totalCount - amount;
-		balances[ownerAddress] = balances[ownerAddress] - amount;
+        emit Transfer(address(0), ownerAddress, amount);
+    }
 
-		emit Transfer(ownerAddress, address(0), amount);
-	}
+    function burn(address ownerAddress, uint256 amount) internal onlyOwner {
+        require(amount <= balances[ownerAddress], "Tokens balance is less than burning tokens value");
+
+        totalCount = totalCount - amount;
+        balances[ownerAddress] = balances[ownerAddress] - amount;
+
+        emit Transfer(ownerAddress, address(0), amount);
+    }
 }
